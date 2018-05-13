8b0c58f98d18c6c131135b7b0d615122b7e70092
alex <browna@awty.org>
2018-05-13 15:37:07 -0500

    Revert "README now contains status"
    This reverts commit b02f10dd7673c1b25ea0214a92f296ff891839a7.

--- src/sorting/shell_sort.c
+++ src/sorting/shell_sort.c
@@ -3,17 +3,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
+float n=11;
+int arr[11];
 int main(){
-	// fill array randomly
 	srand(time(NULL));
 	for(int i=0;i<n;i++){
 		arr[i] = rand()%n;
 	}
-	// actual algorithm
-	
-	// print out array
-	for(int k=0;k<n;k++){
-		printf("%d ", arr[k]);
-	}
-	printf("\n");
 }
