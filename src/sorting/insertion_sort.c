b02f10dd7673c1b25ea0214a92f296ff891839a7
alex <browna@awty.org>
2018-05-13 15:37:07 -0500

    README now contains status

--- src/sorting/insertion_sort.c
+++ src/sorting/insertion_sort.c
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
-float n=11;
-int arr[11];
 int main(){
+	// fill array randomly
 	srand(time(NULL));
 	for(int i=0;i<n;i++){
 		arr[i] = rand()%n;
 	}
+	// actual algorithm
+	
+	// print out array
+	for(int k=0;k<n;k++){
+		printf("%d ", arr[k]);
+	}
+	printf("\n");
 }
