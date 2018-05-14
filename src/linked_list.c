f9427e29cb0e93327fc3b2b33b00c6e9b9a2549a
alex <browna@awty.org>
2018-05-14 10:11:07 -0500

    linked_list: added traverse and insert

--- src/linked_list.c
+++ src/linked_list.c
@@ -17,6 +17,19 @@ void insert(struct node *to_insert,struct node *before,struct node *after){
 	*before->next = *to_insert;
 	*to_insert->next = *after;
 }
+void prepend(struct node *head,struct node *to_prepend){
+	*to_prepend->next = *head;
+}
+void traverse(struct node *head){
+	struct node next_up = *head;
+	while(1==1){
+		printf("%d ", *next_up.val);
+		if(!*next_up.next){
+			break;
+		}
+	}
+	printf("\n");
+}
 int main(){
 	struct node head = create(10);
 	struct node body_0 = create(1);
@@ -24,4 +37,5 @@ int main(){
 	append(&body_0,&head);
 	insert(&middle,&head,&body_0);
 	printf("%d\n",head.next->val);
+	traverse(&head);
 }
