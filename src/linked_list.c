ce213630f39257e1e3b894495e7130d0e04e1a27
alex <browna@awty.org>
2018-05-14 07:25:16 -0500

    added insert function to linked_list & compiled

--- src/linked_list.c
+++ src/linked_list.c
@@ -13,9 +13,15 @@ struct node create(int value){
 void append(struct node *that,struct node *to_that){
 	*to_that->next = *that;
 }
+void insert(struct node *to_insert,struct node *before,struct node *after){
+	*before->next = *to_insert;
+	*to_insert->next = *after;
+}
 int main(){
 	struct node head = create(10);
 	struct node body_0 = create(1);
+	struct node middle = create(5);
 	append(&body_0,&head);
+	insert(&middle,&head,&body_0);
 	printf("%d\n",head.next->val);
 }
